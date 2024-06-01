31b67d6cadabaa60437f0a6ac59e27f71a8c8a49
ciadconi <Cas.DeConinck@UGent.be>
2024-06-01 21:09:06 +0200

    terraform s3 and provider file

---
+++ infra/provider.tf
@@ -0,0 +1,18 @@
+terraform{
+  required_version = "~>1.8.0"
+  required_providers {
+    aws={
+      source="hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+  backend "s3"{
+    bucket = "mlops-bucket-cdc"
+    key = "tfstate/terraform.tfstate"
+    dynamodb_table = "mlopslock"
+    region = "eu-west-1"
+  }
+}
+provider "aws" {
+  region = "eu-west-1"
+}
